1e6e1c8bef9f9fa21fcf155b354b0d119fc69208
TimelyToga <tblumberg39@gmail.com>
2016-10-16 20:39:39 -0400

    Initial version of EMR for the input yellow taxi dataset

--- test_parser.py
+++ test_parser.py
@@ -1,7 +1,14 @@
 import csv, sys
+import calendar
 from geohash import encode
+from datetime import datetime
+
 big_file = 'data/yellow_tripdata_2016-01.csv'
 example_file = 'yellow_tripdata_example.csv'
+
+output_file = 'geography.csv' # Come up with another name
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 # lat = 40.67956543
 # long_coord = -73.98455048
@@ -10,19 +17,28 @@ example_file = 'yellow_tripdata_example.csv'
 #
 # print my_geohash
 
-with open(big_file, 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        try:
-            pickup_gh = encode(float(row[6]), float(row[5]))
-            if not pickup_gh.startswith("dr"):
-                continue
+with open(example_file, 'rb') as f:
+    with open(output_file, 'w') as of:
+        reader = csv.reader(f)
+        writer = csv.writer(of, delimiter=',')
+        for row in reader:
+            try:
+                start_time = datetime.strptime(row[1], DATE_FORMAT)
+                start_time_utc = calendar.timegm(start_time.utctimetuple())
 
-            dropoff_gh = encode(float(row[10]), float(row[9]))
-            if not dropoff_gh.startswith("dr"):
-                continue
+                pickup_gh = encode(float(row[6]), float(row[5]))
+                if not pickup_gh.startswith("dr"):
+                    continue
 
-            print("START: {}, END: {}".format(pickup_gh, dropoff_gh))
-        except Exception:
-            # Make sure that the data in the lat / long columns are floats
-            continue
+                dropoff_gh = encode(float(row[10]), float(row[9]))
+                if not dropoff_gh.startswith("dr"):
+                    continue
+
+
+                writer.writerow([start_time_utc, pickup_gh, dropoff_gh])
+
+                print("START: {}, END: {}".format(pickup_gh, dropoff_gh))
+            except Exception as exc:
+                # Make sure that the data in the lat / long columns are floats
+                print exc
+                continue
