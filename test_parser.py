cf5ad68e91089b1c93b15dc6e73f7d8406cdecd5
TimelyToga <tblumberg39@gmail.com>
2016-10-16 20:07:06 -0400

    Initial geohash

--- test_parser.py
+++ test_parser.py
@@ -1,6 +1,15 @@
 import csv, sys
+from geohash import encode
 filename = 'data/yellow_tripdata_2016-01.csv'
-with open(filename, 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-    	print row
+
+lat = 40.67956543
+long_coord = -73.98455048
+
+my_geohash = encode(lat, long_coord)
+
+print my_geohash
+
+# with open(filename, 'rb') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#     	print row
