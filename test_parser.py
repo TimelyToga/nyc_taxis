35fbf76a89f7d9dc37b825acd8fed78fe6327be3
TimelyToga <tblumberg39@gmail.com>
2016-10-16 20:21:29 -0400

    Both geohashes are parsed out

--- test_parser.py
+++ test_parser.py
@@ -1,15 +1,28 @@
 import csv, sys
 from geohash import encode
-filename = 'data/yellow_tripdata_2016-01.csv'
+big_file = 'data/yellow_tripdata_2016-01.csv'
+example_file = 'yellow_tripdata_example.csv'
 
-lat = 40.67956543
-long_coord = -73.98455048
+# lat = 40.67956543
+# long_coord = -73.98455048
+#
+# my_geohash = encode(lat, long_coord)
+#
+# print my_geohash
 
-my_geohash = encode(lat, long_coord)
+with open(big_file, 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        try:
+            pickup_gh = encode(float(row[6]), float(row[5]))
+            if not pickup_gh.startswith("dr"):
+                continue
 
-print my_geohash
+            dropoff_gh = encode(float(row[10]), float(row[9]))
+            if not dropoff_gh.startswith("dr"):
+                continue
 
-# with open(filename, 'rb') as f:
-#     reader = csv.reader(f)
-#     for row in reader:
-#     	print row
+            print("START: {}, END: {}".format(pickup_gh, dropoff_gh))
+        except Exception:
+            # Make sure that the data in the lat / long columns are floats
+            continue
